6c44e3e9a53e5132c63e2603f087b683b363a32d
Jasmine T <tanghocle456@gmail.com>
2023-05-26 01:20:07 -0700

    Refactor commands of Oikos into 1 single file and update oikosbot.py

--- oikosbot.py
+++ oikosbot.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 
 import event_handlers
-
+import oikos_commands
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
@@ -20,19 +20,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 command_center_channel_id = 1111557645720096848
 
 event_handlers.setup_events(bot)
-
-@bot.command()
-@commands.has_any_role("Oikos Controller")
-async def say_as_oikos(ctx, arg, channel_id = 1101196152809992264):
-
-    target_channel = bot.get_channel(channel_id)
-
-    if ctx.channel.id == command_center_channel_id:
-        if(target_channel is not None):
-            await target_channel.send(arg)
-    else:
-        await ctx.send("This command can only be used in the command-center")
-
+oikos_commands.setup_commands(bot)
 
 @bot.event
 async def on_message(message):
